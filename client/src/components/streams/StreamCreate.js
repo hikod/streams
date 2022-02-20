e69191dd97e3c04fb89a476f36f38281059eaf2e
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-02-19 19:29:36 -0500

    adding api calls

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -25,7 +27,9 @@ class StreamCreate extends React.Component {
       </div>
     );
   };
-  onSubmit(formValues) {}
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
   render() {
     return (
       <form
@@ -54,8 +58,9 @@ const validate = (formValues) => {
   }
   return errors;
 };
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   //validate: validate,
   validate,
 })(StreamCreate);
+export default connect(null, { createStream })(formWrapped);
