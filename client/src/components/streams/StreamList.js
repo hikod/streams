98cfee1362df5b982412de69fd52ea305477772e
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-02-19 21:52:09 -0500

    adding semantic ui classes and rendering streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
-import stream from "../../apis/stream";
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
+  }
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
   }
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             {stream.title}
@@ -19,16 +30,33 @@ class StreamList extends React.Component {
       );
     });
   }
+
+  renderCreateStream() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to={"/streams/new"} className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateStream()}
       </div>
     );
   }
 }
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
