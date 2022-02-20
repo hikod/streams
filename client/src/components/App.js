1d06790d55f51e0e03f4b6387482240422937ebe
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-02-20 10:30:23 -0500

    refactoring stream edit component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <div>
           <Route path="/" exact component={StreamList}></Route>
-          <Route path="/streams/edit" exact component={StreamEdit}></Route>
+          <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
           <Route path="/streams/delete" exact component={StreamDelete}></Route>
           <Route path="/streams/show" exact component={StreamShow}></Route>
           <Route path="/streams/new" exact component={StreamCreate}></Route>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
