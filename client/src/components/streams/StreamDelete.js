bf14a13df7bfe537b07b068100f23debf4bc5037
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-03-06 14:21:12 -0500

    adding modal component and delete component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,6 +1,12 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return <div></div>;
+  return (
+    <div>
+      Stream Delete
+      <Modal></Modal>
+    </div>
+  );
 };
 export default StreamDelete;
