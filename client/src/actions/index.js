fbe268f4581832b6f43a3e081180e3f74d7ac5d3
hdogrul <hdogrul@HD-MacBook-Pro.local>
2022-03-07 21:34:59 -0500

    redirecting to home page after signing out

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,6 +14,7 @@ export const signIn = (userId) => {
   return { type: SIGN_IN, payload: userId };
 };
 export const signOut = () => {
+  history.push("/");
   return { type: SIGN_OUT };
 };
 export const createStream = (formValues) => async (dispatch, getState) => {
